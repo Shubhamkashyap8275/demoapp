0e7eba0772339923216852df6f738f9c623b7582
raveena.garg <raveena.garg@codebrewinnovations.com>
2024-01-24 16:03:12 +0530

    adding profile image of user

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,33 +1,55 @@
-import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatTabsModule } from '@angular/material/tabs';
-import {MatFormFieldModule} from '@angular/material/form-field';
+import { MatFormFieldModule } from '@angular/material/form-field';
 import { RegistrationComponent } from '../registration/registration.component';
+import { environment } from '../../environments/environment.development';
+import { HttpClient } from '@angular/common/http';
+import { LogService } from '../log.service';
+import { ToastrService } from 'ngx-toastr';
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-login',
   standalone: true,
   imports: [
+    CommonModule,
     MatInputModule,
     MatTabsModule,
     MatButtonModule,
     MatCardModule,
-    RegistrationComponent, 
+    RegistrationComponent,
     MatFormFieldModule,
-    FormsModule],
+    FormsModule, ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
   username: string = '';
   password: string = '';
+  loginForm!: FormGroup
+  constructor(private fb: FormBuilder, private http: HttpClient, private console: LogService, private toster: ToastrService) {
+
+  }
+  ngOnInit(): void {
+    this.loginForm = this.fb.group({
+      'username': [''],
+      'password': ['']
+    })
+  }
 
   login() {
-    // Add your authentication logic here
-    console.log('Username:', this.username);
-    console.log('Password:', this.password);
-    // Replace the console.log statements with your actual authentication logic
+    if (this.loginForm.valid) {
+      this.http.post(environment.apiUrl + 'login', this.loginForm.value).subscribe({
+        next:(resp: any) => {
+          this.console.log('login resp=>', resp)
+          this.toster.success(' Login  Successfully', 'Success')
+        },
+        error:(error: any) => {
+          this.toster.error(error.error.error)
+        }})
+    }
   }
 }
--- src/app/registration/registration.component.ts
+++ src/app/registration/registration.component.ts
@@ -8,18 +8,20 @@ import { MatInputModule } from '@angular/material/input';
 import { environment } from '../../environments/environment.development';
 import { LogService } from '../log.service';
 import { ToastrModule, ToastrService } from 'ngx-toastr';
+import { MatIconModule } from '@angular/material/icon';
 
 
 @Component({
   selector: 'app-registration',
   standalone: true,
-  imports: [FormsModule, MatFormFieldModule, MatInputModule, MatButtonModule, ReactiveFormsModule, CommonModule, HttpClientModule,ToastrModule],
+  imports: [FormsModule, MatFormFieldModule, MatInputModule, MatButtonModule, ReactiveFormsModule, CommonModule, HttpClientModule, ToastrModule, MatIconModule],
   templateUrl: './registration.component.html',
   styleUrls: ['./registration.component.css']
 })
 export class RegistrationComponent implements OnInit {
-
+  previewImage = ''
   registrationForm!: FormGroup;
+  ImageFile: any;
   constructor(private fb: FormBuilder, private http: HttpClient, private console: LogService, private toster: ToastrService) {
 
   }
@@ -29,27 +31,41 @@ export class RegistrationComponent implements OnInit {
       email: ['', Validators.required],
       password: ['', Validators.required]
     })
+    // this.getUser()
   }
   get username() {
     return this.registrationForm.get('username');
-
   }
   get email() {
     return this.registrationForm.get('email');
-
   }
   get password() {
     return this.registrationForm.get('password');
-
+  }
+  onFileChange(event: any) {
+    this.ImageFile=event.target.files[0]
+    this.previewImage=URL.createObjectURL(event.target.files[0])
+    this.console.log('file=>', this.previewImage )
   }
 
   register() {
-    if (this.registrationForm.valid) {
-      this.console.log('registrationForm==>', this.registrationForm)
-      this.http.post(environment.apiUrl + 'register', this.registrationForm.value).subscribe((resp) => {
-        this.toster.success('Regestration is completed successfully','Success')
+    const formData=new FormData()
+    if(this.previewImage){
+      formData.append('userImage',this.ImageFile)
+      formData.append('username',this.registrationForm.controls['username'].value)
+      formData.append('email',this.registrationForm.controls['email'].value)
+      formData.append('password',this.registrationForm.controls['password'].value)
+    }
+    if (this.registrationForm.valid) { 
+      this.http.post(environment.apiUrl + 'register',formData).subscribe((resp) => {
+        this.toster.success('Regestration is completed successfully', 'Success')
       })
     }
-
+  }
+  getUser(){
+    this.http.get(environment.apiUrl + 'getUserDetail').subscribe((resp:any) => {
+     this.console.log("all user==>",resp.user);
+     
+    })
   }
 }
